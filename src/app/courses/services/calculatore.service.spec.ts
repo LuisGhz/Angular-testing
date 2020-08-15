b15b9c0f428370aee23898e2e9d0cbed1ddb7bf1
Luis Gutiérrez <carver.anarquia@gmail.com>
2020-08-15 14:52:34 -0600

    Create fake instance of LoggerService. Call toHaveBennCalledTimes to log method.

--- src/app/courses/services/calculatore.service.spec.ts
+++ src/app/courses/services/calculatore.service.spec.ts
@@ -6,12 +6,16 @@ describe('Calculator service', () => {
     it('Should add two numbers', () => {
         const FIRST_NUMBER: number = 2;
         const SECOND_NUMBER: number  = 2;
-        const calculatorService: CalculatorService = new CalculatorService(new LoggerService());
+
+        const logger = jasmine.createSpyObj('LoggerService', ['log']);
+
+        const calculatorService: CalculatorService = new CalculatorService(logger);
 
 
         const result = calculatorService.add(FIRST_NUMBER, SECOND_NUMBER);
 
         expect(result).toBe(4);
+        expect(logger.log).toHaveBeenCalledTimes(1);
     })
     
     it('Should substract two numbers', () => {
