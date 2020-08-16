add4d8872cb7176c1bd09e3e3c3e0a38e7c3f648
Luis Gutiérrez <carver.anarquia@gmail.com>
2020-08-16 12:30:45 -0600

    Add comments.

--- src/app/courses/services/calculatore.service.spec.ts
+++ src/app/courses/services/calculatore.service.spec.ts
@@ -2,16 +2,29 @@ import { CalculatorService } from './calculator.service';
 import { LoggerService } from './logger.service';
 import { TestBed } from '@angular/core/testing';
 
+
+/**
+ * Add x to describe "xdescribe" will skip this test suit (You can disable individual test adding x "xit")
+ * Add f to describe "fdescribe" will focus in this test suit (You can focus in specific test adding f "fit")
+ */
 describe('Calculator service', () => {
 
     let loggerSpy: any;
     let calculatorService: CalculatorService;
 
     beforeEach(() => {
+        // Create fake instance of LoggserService
         loggerSpy = jasmine.createSpyObj('LoggerService', ['log']);
 
         TestBed.configureTestingModule({
             providers: [
+                /**
+                 *  Use Dependency injection mechanism to provide LoggerService.
+                 *  Why use fake instance of LoggerService?
+                 *  I want test CalculatorService in insolation. Assuming this service works fine with its dependencies.
+                 *  If I use the a LoggerService instance. It will be Integration testing. Because I am checking how both 
+                 *  services work together.
+                 */ 
                 CalculatorService,
                 { provide: LoggerService, useValue: loggerSpy }
             ]
